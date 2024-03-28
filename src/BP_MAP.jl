6ba30ff5bc7682cefb2d871dbe21c6e8a1f5f395
Luiz-Rafael Santos <lrsantos11@gmail.com>
2024-03-28 17:39:52 -0300

    BP_MAP functions

--- src/BP_MAP.jl
+++ src/BP_MAP.jl
@@ -10,43 +10,44 @@ BP_MAP(Affine; itmax=100, EPSVAL=1e-6, verbose=true, x₀=Float64[], kwargs...) 
 """
 function BP_MAP(Affine;
     itmax::Int=100,
-    EPSVAL::Number=1e-6,
+    ε::Number=1e-6,
+    ε_MAP::Number=1e-6,
     verbose::Bool=true,
     x₀::Vector{Float64}=Float64[],
+    BP_solution::Vector{Float64}=Float64[],
     kwargs...)
     m, n = size(Affine.A)
     ProjAffine(x) = ProjectIndicator(Affine, x)
     if isempty(x₀)
         x₀ = zeros(n)
     end
+    BP_solution_given = !isempty(BP_solution)
     xMAP = ProjAffine(x₀)
     distance = norm(xMAP, 2)
     radius = 0.0
     solved = false
     tired = false
     it = 0
-    inner_it = 0
+    inner_it_total = 0
     status = :Tired
-    verbose && @info "m = $m, n = $n"
     while !(solved || tired)
         radius += distance
         BallL1 = IndBallL1(radius)
         Proj_BallL1(x) = ProjectIndicator(BallL1, x)
-        resultMAP = MAP(xMAP, ProjAffine, Proj_BallL1, itmax=itmax, gap_distance=false, kwargs...)
-        inner_it += resultMAP.iter_total
-        zMAP = resultMAP.xApprox
+        zMAP, inner_it, _ = MAP(xMAP, ProjAffine, Proj_BallL1, itmax=itmax, ε=ε_MAP, kwargs...)
+        inner_it_total += inner_it
         xMAP = ProjAffine(zMAP)
-        distance = norm(xMAP - zMAP, 2)
+        BP_solution_given ? distance = norm(xMAP - BP_solution, 2) :  distance = norm(xMAP - zMAP, 2)
         it += 1
         solved = distance < EPSVAL
         if solved
             verbose && @info "solved"
             verbose && @info "it = $it"
-            verbose && @info "inner_it = $inner_it"
+            verbose && @info "inner_it_total = $inner_it_total"
             status = :Solved
         end
         tired = it >= itmax
     end
-    return xMAP, it, inner_it, status
+    return xMAP, it, inner_it_total, status
 end
 
--- src/MAP_utils.jl
+++ src/MAP_utils.jl
@@ -1,3 +1,61 @@
 using LinearAlgebra, SparseArrays
 using ProximalOperators
 
+
+
+"""
+    ProjectIndicator
+"""
+####################################
+"""
+    proj = ProjectIndicator(indicator,x)
+    Projection using Indicator Function from `ProximalOperators.jl`
+    """
+function ProjectIndicator(indicator_func, x)
+    proj, _ = prox(indicator_func, x)
+    return proj
+end
+
+"""
+    MAPiteration!(xMAP,ProjA,ProjectB)
+
+Computes a MAP iteration
+"""
+function MAP_iteration!(xMAP::AbstractArray,
+                        ProjA::AbstractArray,
+                        ProjectB::Function)
+    xMAP .= ProjectB(ProjA)
+    return nothing
+end
+
+"""
+    Method of Alternating Projections
+    MAP(x₀, ProjectA, ProjectB; itmax=100) → xApprox, iter_total
+"""
+
+
+"""
+    MAP(x₀,ProjectA, ProjectB)
+
+    Method of Alternating Projections
+"""
+function MAP(x₀::Vector, ProjectA::Function, ProjectB::Function;
+    ε::Float64=1e-6,
+    itmax::Int=100,
+    xSol::Vector=[])
+    solution_given = !isempty(xSol)
+    iter = 0
+    xMAP = x₀
+    ProjA = ProjectA(xMAP)
+    solved = false
+    tired  =  false
+    while !(solved || tired)
+        MAP_iteration!(xMAP, ProjA, ProjectB)    
+        ProjA = ProjectA(xMAP)
+        solution_given ? tolMAP = norm(xMAP - xSol, Inf) : tolMAP = norm(ProjA - xMAP, Inf)
+        solved = tolMAP < ε
+        iter += 1
+        tired = iter >= itmax
+    end
+    return xMAP, iter, tolMAP
+end
