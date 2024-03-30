5dc8b7e88b32b0c3d92a9079733114b2ef57550c
Paulo J. S. Silva <pjssilva@gmail.com>
2024-03-30 12:34:53 -0300

    Better organization of the BP code using a submodule

--- src/BP.jl
+++ src/BP.jl
@@ -1,19 +1,15 @@
 """
 Defines the Basis Porsuit peoblem type (BPProblem) and auxliary functions.
 """
-
 module BP
-
-export BPProblem
-export readl1test, solvewithLP
 
 using DrWatson
 @quickactivate "BP_MAP"
 
 using LinearAlgebra
-using JuMP
-using HiGHS
-using MAT
+
+export BPProblem
+export readl1test, solvewithLP
 
 """
 A Basis Pursuit problem data: ``\\min_x \\| x \\|_1`` s.t. ``Ax = b``
@@ -60,9 +56,10 @@ struct BPProblem{T<:AbstractFloat}
 end
 
 "Construct a BPproblem without a solution or optimal value"
-function BPProblem(A::AbstractMatrix{T}, b::AbstractVector{T}) where {T<:AbstractFloat}
+function BPProblem(A, b)
+    T = eltype(A)
     Tnan = zero(T) / zero(T)
-    
+
     m, n = size(A)
     sol = fill(Tnan, n)
     optval = Tnan
@@ -70,57 +67,17 @@ function BPProblem(A::AbstractMatrix{T}, b::AbstractVector{T}) where {T<:Abstrac
 end
 
 "Construct a BPProblem computing the optimal value from the given solution"
-BPProblem(
-    A::AbstractMatrix{T},
-    b::AbstractVector{T},
-    sol::AbstractVector{T},
-) where {T<:AbstractFloat} = BPProblem(A, b, sol, norm(sol, 1))
+BPProblem(A, b, sol::AbstractVector) = BPProblem(A, b, sol, norm(sol, 1))
 
 "Construct a BPProblem giving the optimal value but no solution"
-function BPProblem(
-    A::AbstractMatrix{T},
-    b::AbstractVector{T},
-    optval::T,
-) where {T<:AbstractFloat}
-    Tnan = zero(T) / zero(T)
+function BPProblem(A, b, optval::AbstractFloat)
+    Tnan = zero(optval) / zero(optval)
     m, n = size(A)
     sol = fill(Tnan, n)
     return BPProblem(A, b, sol, optval)
 end
 
-"Read a test from the Lorentz, Pfetsch, and Tillmann testset"
-function readl1test(filename)
-    # Verify if the test is available
-    dir = datadir("exp_raw", "L1_Testset_mat")
-    if !isdir(dir)
-        println(dir)
-        @error "Test set is not available"
-    end
-    filename = joinpath(dir, filename)
-    if !isfile(filename)
-        @error "Test file does not exist"
-    end
-
-    data = matread(filename)
-    # The solution is represented as a one column matrix. Get the respective vector instead.
-    return BPProblem(data["A"], data["b"][:, 1], data["x"][:, 1])
-end
-
-"Solve a BPProblem using a regular linear programming solver"
-function solvewithLP(prob::BPProblem, Solver = HiGHS)
-
-    # Create a LP model that represents the basis pursuit problem
-    model = Model(Solver.Optimizer)
-    m, n = size(prob.A)
-    @variable(model, xplus[1:n] >= 0)
-    @variable(model, xminus[1:n] >= 0)
-    @objective(model, Min, sum(xplus) + sum(xminus))
-    @constraint(model, prob.A * xplus - prob.A * xminus == prob.b)
-
-    # Solve the model and return the solution
-    optimize!(model)
-    @assert termination_status(model) == OPTIMAL
-    return value.(xplus) - value.(xminus)
-end
+include("BPUtils.jl")
+using .BPUtils
 
 end
---
+++ src/BPUtils.jl
@@ -0,0 +1,53 @@
+"""
+Utilities for Basis Pursuit problems.
+"""
+
+module BPUtils 
+
+using ..BP
+
+using DrWatson
+@quickactivate "BP_MAP"
+
+using JuMP
+using HiGHS
+using MAT
+
+export readl1test, solvewithLP
+
+"Read a test from the Lorentz, Pfetsch, and Tillmann testset"
+function readl1test(filename)
+    # Verify if the test is available
+    dir = datadir("exp_raw", "L1_Testset_mat")
+    if !isdir(dir)
+        println(dir)
+        @error "Test set is not available"
+    end
+    filename = joinpath(dir, filename)
+    if !isfile(filename)
+        @error "Test file does not exist"
+    end
+
+    data = matread(filename)
+    # The solution is represented as a one column matrix. Get the respective vector instead.
+    return BPProblem(data["A"], data["b"][:, 1], data["x"][:, 1])
+end
+
+"Solve a BPProblem using a regular linear programming solver"
+function solvewithLP(prob::BPProblem, Solver = HiGHS)
+
+    # Create a LP model that represents the basis pursuit problem
+    model = Model(Solver.Optimizer)
+    m, n = size(prob.A)
+    @variable(model, xplus[1:n] >= 0)
+    @variable(model, xminus[1:n] >= 0)
+    @objective(model, Min, sum(xplus) + sum(xminus))
+    @constraint(model, prob.A * xplus - prob.A * xminus == prob.b)
+
+    # Solve the model and return the solution
+    optimize!(model)
+    @assert termination_status(model) == OPTIMAL
+    return value.(xplus) - value.(xminus)
+end
+
+end
