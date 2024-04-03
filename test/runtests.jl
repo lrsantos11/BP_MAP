ba39d88c8e4ebe24e9c32f73dc7a7a4e653807c2
Luiz-Rafael Santos <lrsantos11@gmail.com>
2024-04-03 15:14:05 -0300

    adds example B.2 of [HL14]

--- test/runtests.jl
+++ test/runtests.jl
@@ -26,17 +26,52 @@ using .BP
         1.0 -1.0 1.0 -1.0 -1.0 1.0 -1.0 1
     ] ./ sqrt(8)
     sol = [10.0, 0, 0, 0, 0, 0, 0, 0]
-    probHL14 = BPProblem(sol, A)
-    Affine = IndAffine(probHL14)
+    probB1_HL14 = BPProblem(sol, A)
+    Affine = IndAffine(probB1_HL14)
     tol = 1e-3
     xMAP, it, inner_it, status = BP_MAP(Affine, itmax=itmax, ε = tol, ε_MAP = tol, BP_solution = sol, verbose = true)
+    @info "BP-MAP status is $status with  $(it) iterations and $(inner_it) inner iterations"
     @info "xMAP - sol = $(norm(xMAP - sol, 2))"
     @test status == :Solved
+    @info "Using HOC"
     xSol, _ = heuristic_optimality_check(xMAP, Affine, δ = tol)
     @info "Solucao:  $xSol"
     @info "xSol - sol = $(norm(xSol - sol, 2))" 
     @test xSol ≈ sol
 end
+
+
+##
+@testset "Example B2 [HL2014]" begin
+    @info "Example B.2 of Hesse and Luke 2014"
+    itmax = 500
+    A = [
+        1 -0.5 0
+        0 0.5 -1
+    ]
+    m, n = size(A)
+    b = [-5.0, 5]
+    prob_B2_HL14 = BPProblem(A, b)
+    # This problem has infinite solutions. BPMAP is converging to one of them.  
+    sol = [-10/3, 10/3, -10/3]
+    Affine = IndAffine(prob_B2_HL14)
+    tol = 1e-3
+    xMAP, it, inner_it, status = BP_MAP(
+        Affine,
+        itmax = itmax,
+        ε = tol,
+        ε_MAP = tol,
+        verbose = false,
+    )
+    @info "BP-MAP status is $status with  $(it) iterations and $(inner_it) inner iterations"
+    @info "Solution not unique. Not calling HOC here"
+    @info "xMAP - sol = $(norm(xMAP - sol, 2))"
+    @test status == :Solved
+    @test xMAP ≈ sol
+
+end
+
+
 
 ##
 "Downloads Tests from from the Lorentz, Pfetsch, and Tillmann collection"
