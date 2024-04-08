050dff19d7b9afbad9280aa229d7aa990fbf5a8d
Luiz-Rafael Santos <lrsantos11@gmail.com>
2024-04-08 15:38:21 -0300

    changes  LP to BP_LP.jl

--- src/BP_MAP.jl
+++ src/BP_MAP.jl
@@ -12,8 +12,6 @@ Authors: LRS an PJSS
 module BP_MAP
 
 using LinearAlgebra
-using JuMP
-using HiGHS
 import ProximalOperators: IndBallL1
 
 export solveBP_MAP, solveBP_LP, buildBP_LPModel, solveBP_LPmodel!
@@ -133,35 +131,6 @@ function solveBP_MAP(prob::BPProblem; kwags...)
     x, _, _, status = solveBP_MAP(affine; kwags...)
     @assert status == :Solved
     return x
-end
-
-"Solve a BP problem using Linear Programming"
-function solveBP_LP(prob::BPProblem; solver = HiGHS.Optimizer)
-    model = buildBP_LPModel(prob, solver = solver)
-    return solveBP_LPmodel!(model)
-end
-
-"Build a LP model that describes the Basis Pursuit problem"
-function buildBP_LPModel(prob::BPProblem; solver = HiGHS.Optimizer)
-    # Create an LP model that represents the basis pursuit problem
-    model = Model(solver)
-    set_silent(model)
-
-    m, n = size(prob.A)
-    @variable(model, xplus[1:n] >= 0)
-    @variable(model, xminus[1:n] >= 0)
-    @objective(model, Min, sum(xplus) + sum(xminus))
-    @constraint(model, prob.A * xplus - prob.A * xminus == prob.b)
-
-    # Solve the model and return the solution
-    return model
-end
-
-"Solve an LP model describing BP"
-function solveBP_LPmodel!(model)
-    optimize!(model)
-    @assert termination_status(model) == OPTIMAL
-    return value.(model[:xplus]) - value.(model[:xminus])
 end
 
 end
