db5bc01abcf7a7263891426e4ffbe87a1ba15d58
Luiz-Rafael Santos <lrsantos11@gmail.com>
2024-12-18 14:45:55 -0300

    adds ISAL to runbenchmark.jl

--- test/runbenchmark.jl
+++ test/runbenchmark.jl
@@ -22,6 +22,10 @@ if solvertype == :gurobi
     global gurobi_env = Gurobi.Env()
 end
 
+
+# Include the BP_ISAL1.jl scripts
+include(scriptsdir("BP_ISAL1.jl"))
+
 "Relative error assuming that b is not 0"
 function relerror(a, b)
     return norm(a - b) / norm(b)
